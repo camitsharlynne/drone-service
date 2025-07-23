c445250e0871539658183278a8c6c47efd9af56c
Sharlynne Camit <camitsharlynne@gmail.com>
2025-07-23 22:17:24 +0800

    Added short descriptions to interfaces

--- src/main/java/com/java/droneservice/service/IDroneService.java
+++ src/main/java/com/java/droneservice/service/IDroneService.java
@@ -6,11 +6,33 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+/**
+ * Service interface for DroneService.
+ */
 public interface IDroneService {
 
+    /**
+     * Retrieves the available drones registered.
+     * @return DroneDTO
+     */
     List<DroneDTO> getAllDrones();
+
+    /**
+     * Registers drone.
+     * @return DroneDTO list
+     */
     List<DroneDTO> registerDrone(DroneDTO drone);
+
+    /**
+     * Loads medication as payload to the drone.
+     * @return DroneDTO
+     */
     DroneDTO loadMedication(String serialNumber, String medicationPayload, MultipartFile file);
+
+    /**
+     * Retrieves the payloads of a specific drone by its serialNumber.
+     * @return MedicationDTO
+     */
     List<MedicationDTO> getPayload(String droneSerialNumber);
 
 }
--- src/main/java/com/java/droneservice/service/IMedicationService.java
+++ src/main/java/com/java/droneservice/service/IMedicationService.java
@@ -2,7 +2,16 @@ package com.java.droneservice.service;
 
 import org.springframework.core.io.Resource;
 
+/**
+ * Service interface for MedicationService.
+ */
 public interface IMedicationService {
 
+    /**
+     * Retrieves the payload image for the medication.
+     * @param droneSerialNumber - the drone's serial number
+     * @param medicationCode - the medication's code
+     * @return resource/image
+     */
     Resource getMedicationImage(String droneSerialNumber, String medicationCode);
 }
